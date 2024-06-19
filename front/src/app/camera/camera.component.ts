6c4cb7fd1339a8264fb6f3795a68fb5adfc2f305
melissabillard <billardmelissa1@gmail.com>
2024-06-19 19:31:22 +0200

    caméra prêt à être déployé sur le front

--- front/src/app/camera/camera.component.ts
+++ front/src/app/camera/camera.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
 
 export class CameraComponent {
 
+    updateServo(servo: number, direction: string) {
+        
+        console.log(servo + "-" + direction)
 
+        const url = `http://192.168.0.11/moveServo?servo=${servo}&direction=${direction}`;
+        fetch(url)
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
 }
