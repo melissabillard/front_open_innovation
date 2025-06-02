25d2d81bd2f194fee7c0a0adc3fe28de7ead9714
melissa billard <billardmelissa1@gmail.com>
2025-06-02 12:44:22 +0200

    ajouter fonction affichage mot de passe haché

---
+++ back/database/testHash.js
@@ -0,0 +1,18 @@
+const { hashPassword } = require('./passwordUtils');
+
+// Récupère le mot de passe depuis la ligne de commande
+const password = process.argv[2];
+
+if (!password) {
+  console.error('❌ Veuillez fournir un mot de passe :');
+  console.error('Exemple : node testHash.js monMotDePasse123');
+  process.exit(1);
+}
+
+hashPassword(password, (err, hash) => {
+  if (err) {
+    console.error('Erreur de hash :', err);
+  } else {
+    console.log('✅ Mot de passe hashé :', hash);
+  }
+});
