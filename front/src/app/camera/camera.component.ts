9d5e48e06839623a3302b362f1ad5cf83de27ddf
melissa billard <billardmelissa1@gmail.com>
2025-07-08 10:00:32 +0200

    update lien caméra mouvement

--- front/src/app/camera/camera.component.ts
+++ front/src/app/camera/camera.component.ts
@@ -12,7 +12,7 @@ export class CameraComponent {
         
         console.log(servo + "-" + direction)
 
-        const url = `http://192.168.0.11/moveServo?servo=${servo}&direction=${direction}`;
+        const url = `http:// 192.168.0.94:81/moveServo?dir=${servo}&delta=${direction}`;
         fetch(url)
             .then(response => response.text())
             .then(data => console.log(data))
